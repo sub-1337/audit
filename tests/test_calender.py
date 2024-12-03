3ad1ab21b7ff60508e6d57c4ed9783c466f3843f
Maxim <maxim.k.elasnce@gmail.com>
2024-12-03 21:56:17 +0300

    Тесты

--- tests/test_calender.py
+++ tests/test_calender.py
@@ -6,5 +6,12 @@ from core.calendar import calender
 
 def test():
     cal = calender()
-    i = cal.weekday(year = 2024, month = 12, day = 3)
-    print(i)
+
+    days = [[2, 2024, 12, 3],
+            [3, 2024, 12, 4]]
+    for day in days:
+        day_in_fact = cal.weekday(year = day[1], month = day[2], day = day[3])
+        if day[0] == day_in_fact:
+            print("ok", day)
+        else:
+            print("not ok ", "day is ", day_in_fact," expected ", day)

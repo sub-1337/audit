451da414cc073245da3a66681157b12bbb45ab76
Sub <max@sub1337.pro>
2025-01-20 11:16:02 +0300

    Небольшой рефакторинг

--- tests/debug_comb.py
+++ tests/debug_comb.py
@@ -4,77 +4,47 @@ import re
 from enum import Enum
 
 class MainWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        path = "D:\\git\\audit\\data\\1kurs.xlsx"
-
-        # To open the workbook 
-        # workbook object is created
-        wb_obj = openpyxl.load_workbook(path)
-
-        # Get workbook active sheet object
-        # from the active attribute
-        sheet_obj = wb_obj.active
-
-        row = 100
-        col = 25
-
-        self.setWindowTitle("Пример QTableWidget")
-        self.resize(600, 400)
-
-        # Создание таблицы
-        self.table = QTableWidget()
-        self.table.setRowCount(row)  # Количество строк
-        self.table.setColumnCount(col)  # Количество столбцов
-        #self.table.setHorizontalHeaderLabels(["Имя", "Возраст", "Город"])  # Заголовки столбцов
-
-        def getMergedCellVal(sheet, cell):
-            rng = [s for s in sheet.merged_cells.ranges if cell.coordinate in s]
-            return sheet.cell(rng[0].min_row, rng[0].min_col).value if len(rng)!=0 else cell.value
-        #def isDayOfWeek(str):
-        #    if str == "п о н е д е л ь н и к"
-
-        processed = [ [0]*50 for i in range(100)]
-
-
-        # Избавится от объеденённых ячеек
-        def unMergeCells(sheet_obj, processed):
-            for i_row in range(1,row):
-                for i_col in range(1, col):              
-                    cell = sheet_obj.cell(row=i_row, column=i_col)
-                    if isinstance(cell, openpyxl.cell.cell.MergedCell):
-                        value_merged = getMergedCellVal(sheet_obj, cell)
-                        processed[i_row][i_col] = value_merged
-                        #print("cell ",i_row, " ", i_col, " ", )                   
-                    else:
-                        processed[i_row][i_col] = sheet_obj.cell(row =  i_row, column = i_col).value
-                    #    print("This cell is not merged.")
-        
-        unMergeCells(sheet_obj, processed)
-        def detectClass(string):
-            if "пара" in string:
-                return True
-            else:
-                return False
-        def detectTime(string):
-            res = re.search("[+-]?([0-9]*[.])?[0-9]+?\/[+-]?([0-9]*[.])?[0-9]+", string)
-            if res:
-                return True
-            else:
-                return False
-        def detectPartOfGroup(string):
-            if "п/гр" in string:
-                return True
-            else:
-                return False
-        class DirectionOfGroupingUp(Enum):
-            TOP = 0
-            BOTTOM = 1
-            RIGHT = 2
-            LEFT = 3
-        def groupUpBorders(sheet_obj, processed, direction : DirectionOfGroupingUp):
-            def check(i_row_parent, i_col_parent, i_row, i_col, i_row_prev, i_col_prev, is_going, sheet_obj, processed, direction : DirectionOfGroupingUp):
+   
+    def getMergedCellVal(self, sheet, cell):
+        rng = [s for s in sheet.merged_cells.ranges if cell.coordinate in s]
+        return sheet.cell(rng[0].min_row, rng[0].min_col).value if len(rng)!=0 else cell.value
+    
+    def unMergeCells(self, sheet_obj, processed):
+        for i_row in range(1,self.row):
+            for i_col in range(1, self.col):              
+                cell = sheet_obj.cell(row=i_row, column=i_col)
+                if isinstance(cell, openpyxl.cell.cell.MergedCell):
+                    value_merged = self.getMergedCellVal(sheet_obj, cell)
+                    processed[i_row][i_col] = value_merged             
+                else:
+                    processed[i_row][i_col] = sheet_obj.cell(row =  i_row, column = i_col).value
+    
+    def detectClass(self, string):
+        if "пара" in string:
+            return True
+        else:
+            return False
+ 
+    def detectTime(self, string):
+        res = re.search("[+-]?([0-9]*[.])?[0-9]+?\/[+-]?([0-9]*[.])?[0-9]+", string)
+        if res:
+            return True
+        else:
+            return False
+    
+    def detectPartOfGroup(self, string):
+        if "п/гр" in string:
+            return True
+        else:
+            return False
+    class DirectionOfGroupingUp(Enum):
+        TOP = 0
+        BOTTOM = 1
+        RIGHT = 2
+        LEFT = 3
+    
+    def groupUpBorders(self, sheet_obj, processed, direction : DirectionOfGroupingUp):
+            def check(i_row_parent, i_col_parent, i_row, i_col, is_going, sheet_obj, processed, direction : self.DirectionOfGroupingUp):
                 def checkAny(cell):
                     if cell.border.top.style != None or \
                        cell.border.bottom.style != None or \
@@ -91,7 +61,7 @@ class MainWindow(QWidget):
                         return True
                     else:
                         return False
-                def checkSide(cell, direction : DirectionOfGroupingUp):
+                def checkSide(cell, direction : self.DirectionOfGroupingUp):
                     def checkTop(cell):
                         if cell.border.top.style == None:
                             return True
@@ -102,47 +72,82 @@ class MainWindow(QWidget):
                             return True
                         else:
                             return False
-                    if direction == DirectionOfGroupingUp.TOP:
+                    if direction == self.DirectionOfGroupingUp.TOP:
                         return checkTop(cell)
-                    elif direction == DirectionOfGroupingUp.BOTTOM:
+                    elif direction == self.DirectionOfGroupingUp.BOTTOM:
                         return checkBottom(cell)
-
                 
                 cell = sheet_obj.cell(row=i_row, column=i_col)
-                cell_prev = sheet_obj.cell(row=i_row_prev, column=i_col_prev)
                 origin_cell = sheet_obj.cell(row=i_row_parent, column=i_col_parent)
                 if checkAny(cell):
                     if checkAll(cell) == False:
-                        if cell.value != None: # Вот тут ошибка когда рекурсия
-                            if not detectClass(cell.value):
+                        if cell.value != None:
+                            if not self.detectClass(cell.value):
                                 if checkSide(cell, direction):
-                                    if detectTime(cell.value) == False:
-                                        if detectPartOfGroup(cell.value) == False:
-                                            #print(cell.value)
-                                            if i_row < 100 and  i_col < 30:
-                                                if (i_row - 1) > 1:
-                                                    processed[i_row - 1][i_col] = origin_cell.value
+                                    if self.detectTime(cell.value) == False:
+                                        if self.detectPartOfGroup(cell.value) == False:
+                                            if i_row < 100 and  i_col < 30:                                                
+                                                if direction == self.DirectionOfGroupingUp.TOP:
+                                                    if (i_row - 1) > 1:
+                                                        processed[i_row - 1][i_col] = origin_cell.value
+                                                        is_going = True
+                                                        check(i_row_parent, i_col_parent, i_row - 1, i_col, is_going, sheet_obj, processed, direction)
+                                                if direction == self.DirectionOfGroupingUp.BOTTOM:                                                    
+                                                    processed[i_row + 1][i_col] = origin_cell.value
                                                     is_going = True
-                                                    check(i_row_parent, i_col_parent, i_row - 1, i_col, i_row, i_col, is_going, sheet_obj, processed, direction)
-                        else: #cell.value != None:
+                                                    check(i_row_parent, i_col_parent, i_row + 1, i_col, is_going, sheet_obj, processed, direction)
+                        else: #cell.value == None:
                             if checkSide(cell, direction):
                                 if is_going:
-                                    if i_row < 100 and  i_col < 30:
-                                        if (i_row - 1) > 1:
-                                            processed[i_row - 1][i_col] = origin_cell.value
-                                            check(i_row_parent, i_col_parent, i_row - 1, i_col, i_row, i_col, is_going, sheet_obj, processed, direction)
-                            
+                                    if i_row < 100 and  i_col < 30:                                                                                    
+                                        if direction == self.DirectionOfGroupingUp.TOP:
+                                            if (i_row - 1) > 1:
+                                                processed[i_row - 1][i_col] = origin_cell.value
+                                                check(i_row_parent, i_col_parent, i_row - 1, i_col, is_going, sheet_obj, processed, direction)
+                                        if direction == self.DirectionOfGroupingUp.BOTTOM:                                            
+                                            processed[i_row + 1][i_col] = origin_cell.value
+                                            check(i_row_parent, i_col_parent, i_row + 1, i_col, is_going, sheet_obj, processed, direction)                  
+            for i_row in range(1,self.row):
+                for i_col in range(1, self.col):
+                    check(i_row, i_col, i_row, i_col, False, sheet_obj, processed, direction)
+ 
+        
+    def __init__(self):
+        super().__init__()
 
-                                    
-            for i_row in range(1,row):
-                for i_col in range(1, col):
-                    check(i_row, i_col, i_row, i_col, i_row, i_col, False, sheet_obj, processed, direction)
+        path = "D:\\git\\audit\\data\\1kurs.xlsx"
+        self.row = 100
+        self.col = 25
+        # To open the workbook 
+        # workbook object is created
+        wb_obj = openpyxl.load_workbook(path)
 
-        groupUpBorders(sheet_obj, processed, DirectionOfGroupingUp.BOTTOM)
+        # Get workbook active sheet object
+        # from the active attribute
+        sheet_obj = wb_obj.active
+
+        self.setWindowTitle("Пример QTableWidget")
+        self.resize(600, 400)
+
+        # Создание таблицы
+        self.table = QTableWidget()
+        self.table.setRowCount(self.row)  # Количество строк
+        self.table.setColumnCount(self.col)  # Количество столбцов
+        #self.table.setHorizontalHeaderLabels(["Имя", "Возраст", "Город"])  # Заголовки столбцов
+
+        
+        
 
 
-        for i_row in range(1,row):
-            for i_col in range(1, col):
+        # Избавится от объеденённых ячеек
+        processed = [ [0]*50 for i in range(100)]
+        self.unMergeCells(sheet_obj, processed)
+        self.groupUpBorders(sheet_obj, processed, self.DirectionOfGroupingUp.TOP)
+        self.groupUpBorders(sheet_obj, processed, self.DirectionOfGroupingUp.BOTTOM)
+
+
+        for i_row in range(1,self.row):
+            for i_col in range(1, self.col):
                 item = QTableWidgetItem(processed[i_row][i_col])
                 self.table.setItem(i_row - 1, i_col - 1, item)
 

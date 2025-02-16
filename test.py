75cb5416a8a78ed1a289301ce1234f82aee3993e
Sub <max@sub1337.pro>
2025-02-16 14:38:52 +0300

    Тесты для гуя

--- test.py
+++ test.py
@@ -5,3 +5,10 @@ test_cal()
 from tests.test_read_document import test as test_reader
 print("---Test of reader---")
 test_reader()
+
+import sys
+if len(sys.argv) >= 2:
+    if (sys.argv[1] == "--gui"):
+        from tests.test_gui import test as test_gui
+        print("---Test of gui---")
+        test_gui()

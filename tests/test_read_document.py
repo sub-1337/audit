915fdce62042644802769499bf2e8f732e0166d2
Sub <max@sub1337.pro>
2025-05-15 15:27:14 +0300

    подправил тесты

--- tests/test_read_document.py
+++ tests/test_read_document.py
@@ -16,14 +16,19 @@ def test1():
 def test2():
     doc = DocumentReader(os.path.join("tests", "test_files", "test_reader2.xlsx"))
     doc.readDoc({'year' : 2025, 'month' : 10, 'day' : 11})
+    assert (doc.data.processed[1][2] == '') and (doc.data.processed[1][3] == '')
     assert doc.data.processed[2][2] == doc.data.processed[3][2] == doc.data.processed[4][2] == doc.data.processed[5][2] == doc.data.processed[6][2] == 'b d'
-    assert doc.data.processed[2][3] == doc.data.processed[3][3] == doc.data.processed[4][3] == doc.data.processed[5][3] == doc.data.processed[6][3] == "a2b2d2"
-    assert not doc.data.processed[8][2]
-    assert not doc.data.processed[8][3]
-    assert not doc.data.processed[1][2]
-    assert not doc.data.processed[1][3]
+    assert doc.data.processed[2][3] == doc.data.processed[3][3] == doc.data.processed[4][3] == doc.data.processed[5][3] == doc.data.processed[6][3] == 'b2 d2'
+
+    assert doc.data.processed[7][2] == doc.data.processed[8][2] == 'q w'
+
+    assert doc.data.processed[7][3] == doc.data.processed[8][3] == 'w'
+
+    assert not doc.data.processed[9][2]
+    assert not doc.data.processed[9][3]
     print("ok")
 
 def test():
+    
     test1()
     test2()
